7b2cbe2345c8332e3ced5705990ea3e1a8357aa5
shwethamssuchi <114730622+shwethamssuchi@users.noreply.github.com>
2023-03-01 16:47:55 +0530

    Add files via upload

---
+++ Shwetha DBMS/Q-2/Shwetha_DBMSQ2. Supplier.sql
@@ -0,0 +1,8 @@
+INSERT INTO supplier(SUPP_NAME,SUPP_CITY,SUPP_PHONE)
+VALUES
+	("Rajesh Retails","Delhi",'1234567890'),
+	("Appario Ltd.","Mumbai",'2589631470'),
+	("Knome products","Banglore",'9785462315'),
+	("Bansal Retails","Kochi",'8975463285'),
+	("Mittal Ltd.","Lucknow",'7898456532'),
+	("Mittal Ltd.","Lucknow",'7898456532');
